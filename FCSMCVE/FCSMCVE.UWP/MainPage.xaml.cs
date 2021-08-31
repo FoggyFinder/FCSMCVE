fe17bdecd61ee2d6e2cd632c1cc48f5c2985252b
FoggyFinder <FoggyFinder@yandex.ua>
2021-08-31 13:50:55 +0300

    create an empty project

---
+++ FCSMCVE/FCSMCVE.UWP/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FCSMCVE.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            LoadApplication(new FCSMCVE.App());
+        }
+    }
+}
